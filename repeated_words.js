58415e0d4e12e2a5dc0e9000e12878ea3f67942a
Dayme <dayme@DAYME.(none)>
2013-03-02 00:05:09 +0000

    third

--- repeated_words.js
+++ repeated_words.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 });
 
 function generateOutput(contents) {
-  return contents.replace(/____________________/__,'__________________________________');
+  return contents.replace(/\b(\w+)(\s+)\1\b/ig,'$1$2');
 }
 
 function calculate(evt) {
@@ -39,7 +39,7 @@ var entityMap = {
   };
 
 function escapeHtml(string) {
-  return String(string).replace(/_________/g, function (s) {
-    return ____________;
+  return String(string).replace(/[&<>"'\/]/g, function (s) {
+    return entityMap[s];
   });
 
